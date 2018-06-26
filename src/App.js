cf046a173e324b79433002e86067e0d04291978e
Rio <rio.nose@smartsparrow.com>
2018-06-26 10:58:08 -0400

    Update App component to be a const

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Calendar from './components/Calendar';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <h1 className="App-title">February</h1>
-                    <p className="username">Daenerys</p>
-                </div>
-                <Calendar days={29}/>
+const App = () => {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <h1 className="App-title">February</h1>
+                <p className="username">Daenerys</p>
             </div>
-        );
-    }
+            <Calendar days={28}/>
+        </div>
+    );
 }
 
 export default App;
